21ac67a870e7b422be0bba57cdc79cee01595fa4
Gabriel Laroche <gabriel.laroche@nurun.com>
2020-06-07 18:40:49 -0400

    Add social media links to category pages

--- web/pages/category/[slug].js
+++ web/pages/category/[slug].js
@@ -6,6 +6,7 @@ import '../../public/styles/global.scss';
 import CategoryList from "../../components/CategoryList/CategoryList";
 import Layout from "../../components/Layout/Layout";
 import About from "../../components/About/About";
+import { socialMediaQuery } from "../../utils";
 
 const categoriesQuery = `*[_type == "category"] { slug }`;
 
@@ -15,15 +16,16 @@ const query = `
     ...,
     "projects": *[_type == 'project' && references(^._id)]{..., 'imageUrls': images[].asset->url}}[0] | order(_createdAt asc),
   'categories': *[_type == "category"],
-  'about': *[_type == "about"] {..., 'imageUrl': image.asset->url}[0]
+  'about': *[_type == "about"] {..., 'imageUrl': image.asset->url}[0],
+  ${socialMediaQuery}
 }`;
 
 const Category = ({doc}) => {
-    const {currentCategory, categories, about} = doc;
+    const {currentCategory, categories, about, socialMediaLinks} = doc;
 
     return (
 
-        <Layout title={`Vincent Blouin | ${currentCategory.title}`}>
+        <Layout title={`Vincent Blouin | ${currentCategory.title}`} socialMediaLinks={socialMediaLinks}>
             <About content={about} />
             <CategoryList categories={categories} currentCategoryId={currentCategory._id} />
             <ProjectGrid projects={currentCategory.projects} />
